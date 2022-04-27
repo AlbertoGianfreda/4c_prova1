ef7f51097d42165976e679951bb44eb732ebda1a
4CItite <4CI@INF09>
2022-04-27 11:53:36 +0200

    versione 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 /**
  * 
  * @author Gianfreda
- * @version 1.0
+ * @version 2.0
  *
  */
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("progetto di prova GitHub");
+		System.out.println("progetto di prova 2 GitHub");
 	}
 
 }
